caa09f2d3456dca79376644f11cb8867664a9569
Kevin Muñoz <kevinmunoz@MacBook-Pro-de-Kevin.local>
2024-06-29 10:08:48 -0600

    Register y login

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
+from models import Usuario, db, Categoria, categorias_curso, Curso
+# con eso importamos la password hasheada
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import Config
+from flask_session import Session
+# clase de Integrity Error
+from sqlalchemy.exc import IntegrityError
+# importaciones para login
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app= Flask(__name__)
+app.config.from_object(Config)
+
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+db.init_app(app)
+
+#configuraciones para logearse
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
 
 #INDEX
 @app.route("/")
@@ -10,14 +35,68 @@ def index():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        pass
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+
+        print(f"{email, password}")
+
+        if not email or not password:
+            flash("Campos vacios", "danger")
+            return redirect("/login")
+        
+        usuario = Usuario.query.filter_by(email_user=email).first()
+        
+        if usuario is None:
+            flash("Usuario no encontrado", 'danger')
+            return redirect("/login")
+
+        print(f"U: {usuario.password_hash}")
+
+        if not check_password_hash(usuario.password_hash, password):
+            flash("Contraseñas no coinciden", 'warning')
+            return redirect("/login")
+        
+        # si todos los campos y validaciones estan correctos
+        login_user(usuario)
+        flash("Inicio de sesion exitoso", 'success')
+        return redirect("/")
     else:
         return render_template("login.html")
     
 @app.route("/register", methods=["POST","GET"])
 def register():
     if request.method == "POST":
-        pass
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        rol = request.form.get("rol")
+
+        print(f"{username, password}")
+        # si los campos estan vacios
+        if not username or not password:
+            # es danger, porque es para errores
+            flash("Campos vacios", "danger")
+            return redirect("/register")
+        
+        # creamos un nuevo usuario
+        # crear una password segura
+        password_hash = generate_password_hash(password)
+            
+        try: 
+            # si el usuario es nuevo
+            new_user = Usuario(username=username, password_hash=password_hash, email_user=email, rol=rol)
+
+            # guardamos a la base de datos con db
+            db.session.add(new_user)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash("El usuario ya existe", 'danger')
+            return redirect("/register")
+
+        flash("Usuario registrado!", 'success ')
+        return redirect("/")
     else:
         return render_template("register.html")
     
